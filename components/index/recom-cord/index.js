69bd39ecdc0de1f0e166bae12e299c1b8236485c
Basal <8813036+xgh329179227@user.noreply.gitee.com>
2022-11-25 13:58:00 +0800

    feat: 完成了商品详情页面和菜单页面

--- components/index/recom-cord/index.js
+++ components/index/recom-cord/index.js
@@ -1,5 +1,12 @@
 Component({
   data: {},
   properties: {},
-  methods: {}
+  methods: {
+    toDetail() {
+      // console.log(888)
+      wx.navigateTo({
+        url: '/pages/detail/detail?mid=2'
+      })
+    }
+  }
 })
---
+++ pages/detail/detail.js
@@ -0,0 +1,14 @@
+// pages/detail/detail.js
+Page({
+  /**
+   * 页面的初始数据
+   */
+  data: {},
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad(options) {
+    console.log(options)
+  }
+})
